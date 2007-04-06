a856c54d7cfaa38111f9e3fc73d00789d6039060
dmp <dmp@cs.rice.edu>
2007-04-05 18:37:51 -0700

    add template file
    darcs-hash:20070406013751-22c1e-21814c8b5c8643510431c8da855911c8c7b8f546

---
+++ TEMPLATE.cc
@@ -0,0 +1,22 @@
+/* contains various enhancements to the chow algorithm that do not
+ * appear in the original paper.
+ */
+
+/*--------------------------INCLUDES---------------------------*/
+#include "chow_enhancements.h"
+
+/*------------------MODULE LOCAL DECLARATIONS------------------*/
+namespace {
+}
+
+/*--------------------BEGIN IMPLEMENTATION---------------------*/
+namespace Chow::Enhancements {
+
+}//end Chow::Enhancments namespace
+
+/*-------------------BEGIN LOCAL DEFINITIONS-------------------*/
+
+namespace {
+
+}//end anonymous namespace
+
