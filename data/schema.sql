0839c2a09d33754d34843e0689a63d88f6acb58d
dmp <dmp@cs.rice.edu>
2007-04-08 01:12:14 -0700

    added data  loader for benchmarks
    darcs-hash:20070408081214-22c1e-160a263ce10878adb2b1b228fb53684228f8a252

---
+++ data/schema.sql
@@ -0,0 +1,40 @@
+--chow run data
+--sqlite3 -init schema.sql chow.db 
+
+CREATE TABLE benchmarks(
+  id                     INTEGER PRIMARY KEY,
+  program                VARCHAR,
+  family                 VARCHAR,
+  optimization_id        INTEGER, --foreign key
+  allocation_detail_id   INTEGER, --foriegn key
+  created_on   datetime
+);
+
+CREATE TABLE optimizations(
+  id                     INTEGER PRIMARY KEY,
+  pre_passes             VARCHAR,
+  post_passes            VARCHAR
+);
+
+CREATE TABLE allocation_details(
+  id                             INTEGER PRIMARY KEY,
+  --general
+  algorithm                      VARCHAR,
+  param_string                   VARCHAR,
+  num_registers                  INTEGER,
+  --chow specific
+  bb_size                        INTEGER,
+  partitioned                    BOOLEAN,
+  load_store_movement            BOOLEAN,
+  enhanced_load_store_movement   BOOLEAN
+);
+
+CREATE TABLE timings(
+  id                             INTEGER PRIMARY KEY,
+  function_name                  VARCHAR,
+  instruction_count              INTEGER,
+  calls                          INTEGER,
+  benchmark_id                   INTEGER --foreign key
+);
+
+
